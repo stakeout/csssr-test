b007f44466192a0570aea68cc75a66fc57ca98f5
Vadim Novash <novash@tut.by>
2016-06-07 20:37:56 +0300

    ready. v.1.0.0

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,16 @@
 import svg4everybody from 'svg4everybody';
 import $ from 'jquery';
+import noUiSlider from 'nouislider';
 
 $(() => {
 	svg4everybody();
+});
+const slider = document.getElementById('rangeSlider');
+noUiSlider.create(slider, {
+	start: [35],
+	connect: 'lower',
+	range: {
+		min: 0,
+		max: 100
+	}
 });
