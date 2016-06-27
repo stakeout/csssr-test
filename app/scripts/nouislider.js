cf3cdd901b75b59dedf8494b6c07c5416a28f9f8
Vadim Novash <novash@tut.by>
2016-06-27 16:51:29 +0300

    v.1.1.0

--- app/scripts/nouislider.js
+++ app/scripts/nouislider.js
@@ -11,10 +11,17 @@ noUiSlider.create(slider, {
 });
 const handle = slider.querySelector( '.noUi-handle' );
 const base = slider.querySelector( '.noUi-base' );
-
+//	show value
+function jsVal() {
+	const jsValue = document.querySelector('.js-level__level-value');
+	const value = Number( slider.noUiSlider.get() );
+	jsValue.innerHTML = Math.round(value);
+	return;
+}
 handle.setAttribute( 'tabindex', 0 );
 base.addEventListener( 'click', function () {
 	handle.focus();
+	jsVal();
 });
 handle.addEventListener( 'click', function () {
 	this.focus();
@@ -24,17 +31,18 @@ handle.addEventListener( 'keydown', function ( e ) {
 	const value = Number( slider.noUiSlider.get() );
 
 	switch ( e.which ) {
-		case 37: slider.noUiSlider.set( value - 10 );
+		case 37: slider.noUiSlider.set( value - 5 );
 			break;
-		case 39: slider.noUiSlider.set( value + 10 );
+		case 39: slider.noUiSlider.set( value + 5 );
 			break;
 	}
+	jsVal();
 });
 base.addEventListener( 'wheel', function (e) {
-
 	const value = Number( slider.noUiSlider.get() );
 	const delta = e.deltaY || e.detail || e.wheelDelta;
 	e.preventDefault();
 	handle.focus();
 	slider.noUiSlider.set( value + delta / 10 );
+	jsVal();
 });
